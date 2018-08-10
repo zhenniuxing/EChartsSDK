6e7d703162d7b661c457f9bb7f5829cc24812b81
e <zhenniuxing@163.com>
2018-08-10 09:58:15 +0800

    增加custom图表类

---
+++ EChartsSDK/ECharts/Entities/series/Custom.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECharts.Entities.series
+{
+    public class Custom : Rectangular<Custom>
+    {
+
+        public object renderItem { get; set; }
+
+        public object dimensions { get; set; }
+
+        public object encode { get; set; }
+        
+        public Custom()
+        {
+            this.type = ChartType.custom;
+        }
+        public Custom RenderItem(object renderItem)
+        {
+            this.renderItem = renderItem;
+            return this  ;
+        }
+
+        public Custom Dimensions(IList<string> dimensions)
+        {
+            this.dimensions = dimensions;
+            return this;
+        }
+
+        public Custom Dimensions(params string[] dimensions)
+        {
+            this.dimensions = dimensions.ToList();
+            return this;
+        }
+
+        public Custom Encode(object encode)
+        {
+            this.encode = encode;
+            return this;
+        }
+        
+
+    }
+}
